4af3cb1832c475fe53754b0d31dc2fbc1e940169
Eric Ho <endonoho@gmail.com>
2022-03-30 22:26:39 -0700

    Update wrapper styles

--- src/components/CardWrapper.js
+++ src/components/CardWrapper.js
@@ -4,7 +4,6 @@ import { Card, styled } from '@mui/material';
 import { BLACK } from '../constants/colors';
 
 const StyledCard = styled(Card)({
-	minWidth: 345,
 	borderRadius: '10px',
 	border: `${BLACK} 1px solid`,
 });
