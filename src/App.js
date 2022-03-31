8b3806d90d433e338b722ccb584419ed339cd1a3
Eric Ho <endonoho@gmail.com>
2022-03-30 22:27:35 -0700

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 
-import Header from './components/Content.js';
+import Content from './components/Content.js';
 import TextInput from './components/TextInput.js';
 import useAppData from './hooks/useAppData.js';
 import CardWrapper from './components/CardWrapper.js';
+import Result from './components/Result.js';
+import CheckButton from './components/CheckButton.js';
 
 const App = () => {
-	const { state, handleChange } = useAppData();
-	const { values } = state;
+	const { state, handleChange, handleClick } = useAppData();
+	const { values, text, error } = state;
 
   return (
     <Grid container sx={{ justifyContent: 'center' }}>
-			<CardWrapper>
-				<Header />
-				<TextInput
-					placeholder='xxxx xxxx xxxx xxxx'
-					onChange={handleChange}
-					value={values}
-				/>
-			</CardWrapper>
+			<Box>
+				<CardWrapper>
+					<Content />
+					<TextInput
+						error={error}
+						placeholder='xxxx xxxx xxxx xxxx'
+						value={values}
+						onChange={handleChange}
+					/>
+					{ text && <Result text={text} error={error} /> }
+				</CardWrapper>
+				<CheckButton onClick={handleClick} />
+			</Box>
     </Grid>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,26 +1,20 @@
 import React from 'react';
 
-import { CardContent, Grid, styled, Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import Logo from './Logo';
-
-const StyledCardContent = styled(CardContent)({
-	display: 'flex',
-	flexDirection: 'column',
-	justifyContent: 'center',
-	backgroundColor: '#EFE6DD',
-});
+import ContentWrapper from './ContentWrapper';
 
 const Content = () => {
 
   return (
-		<StyledCardContent>
-			<Grid container sx={{ justifyContent: 'space-between' }}>
+		<ContentWrapper>
+			<Grid container sx={{ justifyContent: 'space-between', marginLeft: '0' }}>
 				<Typography variant='h6'>Balance Checker</Typography>
 				<Logo />
 			</Grid>
 			<Typography variant='body2'>Enter your card number to check it's balance</Typography>
-		</StyledCardContent>
+		</ContentWrapper>
   );
 }
 
