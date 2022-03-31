8e57128eb7fc055ad118bddf975e4cb1372bb544
Eric Ho <endonoho@gmail.com>
2022-03-30 22:26:28 -0700

    Add colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,6 +1,12 @@
 // Main colors for the app
 export const BLACK = '#000000';
 
+// Card
+export const LIGHT_BROWN = '#EFE6DD';
+export const DESERT_STORM = '#EEEBE8';
+
 // Text input
 export const GREY = '#292929';
 export const LIGHT_GREY = '#3E342B80';
+export const DARK_GREEN = '#1E7500';
+export const DARK_ORANGE = '#FF4D00';
