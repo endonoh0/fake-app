cac65f374cee399ca9d72da24b7f80e7363a7925
Eric Ho <endonoho@gmail.com>
2022-03-30 17:26:25 -0700

    Format

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './styles/index.css';
+
 import App from './App';
+
 import { ThemeProvider } from '@mui/system';
 import theme from './styles/theme';
 
