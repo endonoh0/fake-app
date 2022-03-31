f530148c6f9d4145846dc00d9d15a4db7a20ca9e
Eric Ho <endonoho@gmail.com>
2022-03-30 22:28:19 -0700

    Remove focus (WIP)

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,21 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { TextField, styled } from '@mui/material';
-import { BLACK, GREY, LIGHT_GREY } from '../constants/colors';
+
+import { BLACK, DARK_ORANGE, GREY, LIGHT_GREY } from '../constants/colors';
 
 const StyledTextInput = styled(TextField)((props) => ({
-	borderTop: `1px solid ${BLACK}`,
+	borderTop: props.error ? `1px solid ${DARK_ORANGE}` : `1px solid ${BLACK}`,
+	borderBottom: props.error ? `1px solid ${DARK_ORANGE}` : `1px solid ${BLACK}`,
 	'& .MuiOutlinedInput-input': {
 		color: props.value ? GREY : LIGHT_GREY,
-	}
+	},
+	'& .MuiOutlinedInput-notchedOutline': {
+		borderRight: 'unset',
+		borderLeft: 'unset',
+	},
 }));
 
 const TextInput = (props) => {
-
 	return (
 		<StyledTextInput
 			type="text"
 			size='medium'
+			autoFocus
 			fullWidth
 			{...props}
 		/>
@@ -25,7 +31,7 @@ const TextInput = (props) => {
 
 TextInput.propTypes = {
 	value: PropTypes.any.isRequired,
-	onChange: PropTypes.func.isRequired
+	onChange: PropTypes.func.isRequired,
 };
 
 export default TextInput;
