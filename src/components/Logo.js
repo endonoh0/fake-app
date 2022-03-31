5b88396ce47e662a92d6f4c60fd7473f7864e169
Eric Ho <endonoho@gmail.com>
2022-03-30 17:27:59 -0700

    Display Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import DwellingLogo from '../assets/Dwelling-Icon-Filled.svg';
+
+const Logo = () => {
+	return (
+		<img alt='dwelling-logo' src={DwellingLogo} />
+	)
+}
+
+export default Logo;
